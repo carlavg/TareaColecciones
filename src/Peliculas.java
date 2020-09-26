922edde0e3c1ce0d54faf044310335b7a3a2c939
carlavg <vgdaniela514@gmail.com>
2020-09-26 19:13:36 -0300

    Terminado de proyecto java

--- src/Peliculas.java
+++ src/Peliculas.java
@@ -7,19 +7,18 @@ public class Peliculas {
 	public static void main(String[] args) {
 
 		Set<String> listaPeliculas = new HashSet<String>();
-		//Set<String> listaPeliculasCopia = new HashSet<String>();
+		
 
 		listaPeliculas.add("Gladiador");
 		listaPeliculas.add("El transportador");
 		listaPeliculas.add("El señor de los anillos");
 		listaPeliculas.add("Annabelle");
 		listaPeliculas.add("Titanic");
-		listaPeliculas.add("Titanic");
-		
 
+		System.out.println("--------Peliculas--------");
 		System.out.println(listaPeliculas);
 
-		System.out.println("Muestra la lista por separado");
+		System.out.println("Muestra la lista de peliculas por separado");
 		for (String peliculas : listaPeliculas)
 			System.out.println(peliculas);
 
@@ -27,12 +26,35 @@ public class Peliculas {
 
 		boolean estaVacio = listaPeliculas.isEmpty();
 		System.out.println("¿La lista de peliculas en la lista esta vacia?  " + ((estaVacio) ? "Si" : "No"));
-		
+
+		Set<String> listaPeliculasCopia = new HashSet<String>();
+
+		listaPeliculasCopia.add("Gladiador");
+		listaPeliculasCopia.add("El transportador");
+		listaPeliculasCopia.add("El señor de los anillos");
+		listaPeliculasCopia.add("Annabelle");
+		listaPeliculasCopia.add("Titanic");
+
+		for (String pelicula : listaPeliculasCopia) {
+			listaPeliculas.add(pelicula);
+		}
+
+		System.out.println("Ahora vamos a mostrar la lista de peliculas clonada\n" + listaPeliculasCopia);
+
+		System.out.println("Ahora vamos a comparar la lista de peliculas con la nueva lista clonada: ");
+
+		if (listaPeliculas.equals(listaPeliculasCopia)) {
+			System.out.println("Las listas de peliculas son iguales");
+		} else {
+			System.out.println("Las listas de peliculas no son iguales");
+
+		}
 
 		listaPeliculas.clear();
 		System.out.println("Lista de peliculas despues de haber eliminado " + listaPeliculas.size());
 
-		System.out.println("¿La lista de peliculas en la lista esta vacia?  " + listaPeliculas.isEmpty());
+		listaPeliculas.isEmpty();
+		System.out.println("¿La lista de peliculas en la lista esta vacia?  " + ((estaVacio) ? "Si" : "No"));
 
 	}
 }
