5ebf5d802185323706093e1ee2a82060b8ef7da2
Gerardo Budzovsky <gbudzovsky@k12.com>
2020-10-04 01:39:13 -0300

    Terminado el ejercio de HashMap

--- src/AgendaTelefonica.java
+++ src/AgendaTelefonica.java
@@ -6,67 +6,93 @@ public class AgendaTelefonica {
 
 	public static void main(String[] args) {
 
-		Map<String, Integer> agenda_Telefonica = new HashMap<>();
+		Map<String, Integer> agendaTelefonica = new HashMap<String,Integer>();
 
-		agenda_Telefonica.put("Carlos", 4443321);
-		agenda_Telefonica.put("Gerardo", 4443322);
-		agenda_Telefonica.put("Daniela", 4443323);
-		agenda_Telefonica.put("Sandra", 4443324);
-		agenda_Telefonica.put("Osvaldo", 4443329);
-		agenda_Telefonica.put("Santiago", 4443330);
+		agendaTelefonica.put("Carlos", 4443321);
+		agendaTelefonica.put("Gerardo", 15572404);
+		agendaTelefonica.put("Daniela", 15437137);
+		agendaTelefonica.put("Sandra", 4443324);
+		agendaTelefonica.put("Osvaldo", 4443329);
+		agendaTelefonica.put("Santiago", 4443330);
 
-		System.out.println(agenda_Telefonica);
+		
+		System.out.println("Agenda telefónica original");
+		System.out.println(agendaTelefonica);
 
-		int tamano = agenda_Telefonica.size();
-		System.out.println("Tamaño del mapa : " + tamano);
+		int tamanoDelMapa = agendaTelefonica.size();
+		System.out.println("Tamaño del mapa : " + tamanoDelMapa);
 
-		Integer telefono = agenda_Telefonica.get("Carlos");
-		System.out.println("Telefono de Carlos es : " + telefono);
+		System.out.println("El télefono de Carlos es : " + agendaTelefonica.get("Carlos"));
 
-		if (agenda_Telefonica.containsKey("Gerardo")) {
-
-			System.out.println("El telefono de este contacto es:  " + agenda_Telefonica.get("Gerardo"));
+		String key = "Gerardo";
+		System.out.println("Ahora vamos a ver si existe un teléfono agendado para el siguiente nombre: "+ key);
+		if (agendaTelefonica.containsKey(key)) {
+			System.out.println("Si, el telefono de este contacto ("+key+") es:  " + agendaTelefonica.get(key));
 		} else {
-			System.out.println("el contacto no existe");
+			System.out.println("No hay número de telefono asociado a este nombre");
+
+		}
+		
+	    key = "Mariano";
+		System.out.println("Ahora vamos a ver si existe un teléfono agendado para el siguiente nombre: "+ key);
+		if (agendaTelefonica.containsKey(key)) {
+			System.out.println("Si, el telefono de este contacto ("+key+") es:  " + agendaTelefonica.get(key));
+		} else {
+			System.out.println("No hay número de telefono asociado a este nombre");
 
 		}
 
-		if (agenda_Telefonica.containsKey("Mariano")) {
+		Integer value = 15572404;
+		System.out.println("Ahora vamos a ver si existe un nombre asociado al siguiente número: " + value);
+		if (agendaTelefonica.containsValue(value)) {
+			System.out.println("Si, existe un nombre asociado a este número de teléfono ("+ value +")");
+		} else {
+			System.out.println("No existe un nombre asociado a este número de teléfono");
 
-			System.out.println("El telefono de este contacto es:  " + agenda_Telefonica.get("Mariano"));
+		}
 		
-		System.out.print("El contacto no existe ");
+	    value = 12345678;
+		System.out.println("Ahora vamos a ver si existe un nombre asociado al siguiente número: " + value);
+		if (agendaTelefonica.containsValue(value)) {
+			System.out.println("Si, existe un nombre asociado a este número de teléfono ("+ value +")");
+		} else {
+			System.out.println("No existe un nombre asociado a este número de teléfono");
 
-		if (agenda_Telefonica.containsValue(778788));
+		}
 		
-		System.out.print("El contacto existe ");
-		}else {
-			System.out.print("El contacto no existe ");	
-		
-			
-		
-		
-		
-		// clonar el mapa
+	   System.out.println("Ahora vamos a convertir nuestro mapa (Map) en un conjunto (Set) y mostrarlo por consola.");	
+       Set set = agendaTelefonica.entrySet();
+       System.out.println(set);
 
-		 Map<String, Integer> agenda_TelefonicaCopy = new HashMap<>();
-		 
-		
-		// Set<Entry<String,String>> set = map.entrySet();
+		System.out.println("Ahora vamos a clonar el mapa original y mostrarlo por consola.");
+		Map<String,Integer> agendaTelefonicaCopia = new HashMap<String,Integer>();
+        agendaTelefonicaCopia.putAll(agendaTelefonica);
+        System.out.println("Clon del mapa original");
+        System.out.println(agendaTelefonicaCopia);
+        
+        System.out.println("Ahora vamos a comparar el mapa original y su clon para ver si son iguales");
+        
+        if (agendaTelefonica.equals(agendaTelefonicaCopia)) {
+			System.out.println("Los mapas son iguales");
+		} else {
+			System.out.println("Los mapas no son iguales");
+		}
 
-		
-	// vaciar mapa
-		agenda_Telefonica.clear();
+        boolean estaVacio = agendaTelefonica.isEmpty();
+        System.out.println("¿El mapa original está vacío? "+ ((estaVacio) ? "Si" : "No"));
+        
+        System.out.println("Ahora vamos a vaciar el mapa original y mostrarlo por consola");
+        agendaTelefonica.clear();
+		System.out.println(agendaTelefonica);		
+		System.out.println("Tamaño del mapa original despues de haberlo vaciado: " + agendaTelefonica.size());
+		estaVacio = agendaTelefonica.isEmpty();
+        System.out.println("¿El mapa original está vacío? "+ ((estaVacio) ? "Si" : "No"));
+		System.out.println("Ahora vamos a comparar el mapa original vaciado con su clon");
+		        if (agendaTelefonica.equals(agendaTelefonicaCopia)) {
+			System.out.println("Los mapas son iguales");
+		} else {
+			System.out.println("Los mapas no son iguales");
+		}
 
-		
-		//Mostrar el mapa vacio
-		
-		System.out.println("Agenda telefonica despues de haberla vaciado: \n" + agenda_Telefonica);
-		
-		//Mostrar el tamaño del mapa
-		
-		System.out.println("Tamaño de la agenda telefonica despues de haberla vaciado: " + agenda_Telefonica.size());
-
-	}
 }
 }
